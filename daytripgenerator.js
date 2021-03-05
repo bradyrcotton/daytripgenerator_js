e7eaa32171618764349619aa0cb518fb5c254a9e
unknown <bradyrcotton@gmail.com>
2021-03-05 12:33:35 -0600

    added promt to allow user to re-select destination

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -7,14 +7,29 @@ let entertainment = ["Movies", "Mall", "Fishing"];
 function GetRandomValueFromArray(array){
 
     let randomNumber= (Math.floor(Math.random() * array.length));
-    console.log(array[randomNumber]);
+    return array[randomNumber];
  
 
 
 }
-GetRandomValueFromArray(destination);
-GetRandomValueFromArray(restaurant);
-GetRandomValueFromArray(transportation);
-GetRandomValueFromArray(entertainment);
+userDestination= GetRandomValueFromArray(destination);
+userRestaurant= GetRandomValueFromArray(restaurant);
+userTransportation= GetRandomValueFromArray(transportation);
+userEntertainment= GetRandomValueFromArray(entertainment);
 
+let userDestinationInput= prompt("Are you happy with the destination "+userDestination+ "? Enter: Yes or No");
+        if (userDestinationInput === "Yes" || userDestinationInput === "yes"){
+             userDestinationInput = "Comlete";
+        }
+        else if(userDestinationInput === "No" || userDestinationInput === "no"){
+            for (let i = "no"; userDestinationInput != "Complete"; i++){
+            let newDestination = GetRandomValueFromArray(destination);
+            let newDestinationInput= prompt("How does " +newDestination+ " sound? Yes or No");  
+                if (newDestinationInput=== "Yes" || newDestinationInput === "yes"){ 
+                    userDestinationInput = "Complete";
+                       
+               }     
+        }
+    }
 
+           console.log(userDestinationInput);
