5151f181792b0be8e29a71779f01e5e1a86b8ec7
unknown <bradyrcotton@gmail.com>
2021-03-05 21:02:21 -0600

    cleaned up code and refactored redundant code to work in a function

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -12,43 +12,122 @@ function GetRandomValueFromArray(array){
 
 
 }
-userDestination= GetRandomValueFromArray(destination);
+let userDestination= GetRandomValueFromArray(destination);
 
 
-let userDestinationInput= prompt("Are you happy with the destination "+userDestination+ "? Enter: Yes or No");
-        if (userDestinationInput === "Yes" || userDestinationInput === "yes"){
-             userDestinationInput = "Complete";
+function userInput (varible,array){
+
+ let InitialInput =prompt("Are you happy with "+varible+ "? Enter: Yes or No");
+
+ 
+
+        while(InitialInput != "Yes" && InitialInput !="yes" && InitialInput !="No" && InitialInput !="no"){
+        alert("Invalid Response: Please enter Yes or No");
+        let newInput=prompt("Are you happy with "+varible+ "? Enter: Yes or No")
+        InitialInput = newInput;
+}
+        if (InitialInput === "Yes" || InitialInput === "yes"){
+            InitialInput = "Complete";
         }
-        else if(userDestinationInput === "No" || userDestinationInput === "no"){
-            for (let i = "no"; userDestinationInput != "Complete"; i++){
-            let newDestination = GetRandomValueFromArray(destination);
-            let newDestinationInput= prompt("How does " +newDestination+ " sound? Yes or No");  
-                if (newDestinationInput=== "Yes" || newDestinationInput === "yes"){ 
-                    userDestinationInput = "Complete";
+        else if(InitialInput === "No" || InitialInput === "no"){
+            for (let i = "no"; InitialInput != "Complete"; i++){
+            let newDestination = GetRandomValueFromArray(array);// re-randomizes  
+            let newDestinationInput= prompt("How does " +newDestination+ " sound? Yes or No");  // prompts user again
+                if (newDestinationInput=== "Yes" || newDestinationInput === "yes"){ // ends loop when user is ok with choice
+                    InitialInput = "Complete";
                        
                }     
-        }
-    }
+               
 
-           console.log(userDestinationInput);
-userRestaurant= GetRandomValueFromArray(restaurant);
-
-let userRestaurantInput= prompt("Are you happy with the restaurant "+userRestaurant+ "? Enter: Yes or No");
-        if (userRestaurantInput === "Yes" || userRestaurantInput === "yes"){
-             uuserRestaurantInput = "Comlete";
+            }
+            
         }
         
-        else if(userRestaurantInput === "No" || userRestaurantInput === "no"){
-            for (let i = 0; userRestaurantInput != "Complete"; i++){
-            let newRestaurant = GetRandomValueFromArray(restaurant);
-            let newRestaurantInput= prompt("How does " +newRestaurant+ " sound? Yes or No");  
-                if (newRestaurantInput=== "Yes" || newRestaurantInput === "yes"){ 
-                    userRestaurantInput = "Complete";
-                       
-               }     
-        }
-        }
+            return InitialInput;
+}
+let UserDestinationChoice = userInput(userDestination,destination);
+console.log(UserDestinationChoice);
 
-           console.log(userRestaurantInput);
-userTransportation= GetRandomValueFromArray(transportation);
-userEntertainment= GetRandomValueFromArray(entertainment);
+let userRestaurant= GetRandomValueFromArray(restaurant);
+
+let UserRestaurantChoice = userInput(userRestaurant,restaurant);
+console.log(UserRestaurantChoice);
+
+let userTransportation= GetRandomValueFromArray(transportation);
+
+let UserTransportationChoice = userInput(userTransportation,transportation);
+console.log(UserTransportationChoice);
+
+
+
+
+// userRestaurant= GetRandomValueFromArray(restaurant);
+
+// let userRestaurantInput= prompt("Are you happy with the restaurant "+userRestaurant+ "? Enter: Yes or No");
+//         if (userRestaurantInput === "Yes" || userRestaurantInput === "yes"){
+//              userRestaurantInput = "Comlete";
+//         }
+        
+//         else if(userRestaurantInput === "No" || userRestaurantInput === "no"){
+//             for (let i = 0; userRestaurantInput != "Complete"; i++){
+//             let newRestaurant = GetRandomValueFromArray(restaurant); // re-randomizes restaurant
+//             let newRestaurantInput= prompt("How does " +newRestaurant+ " sound? Yes or No");  // prompts user again
+//                 if (newRestaurantInput=== "Yes" || newRestaurantInput === "yes"){ 
+//                     userRestaurantInput = "Complete"; // ends loop when user is ok with choice
+                       
+//                }     
+//         }
+//         }
+
+//            console.log(userRestaurantInput);
+
+
+// userTransportation= GetRandomValueFromArray(transportation);
+
+// let userTransportationInput= prompt("Are you happy with the transportation "+userTransportation+ "? Enter: Yes or No");
+
+            
+//             // while(userTransportationInput !="Yes" || userTransportationInput != "yes" || userTransportationInput != "No" || userTransportationInput != "no"){
+//             //     alert("Invalid Response Please Enter Yes or No");
+//             // let userTransportationInput= prompt("Are you happy with the transportation "+userTransportation+ "? Enter: Yes or No");
+//             // }
+
+//         if (userTransportationInput === "Yes" || userTransportationInput === "yes"){
+//              userTransportationInput = "Comlete";
+//         }
+        
+        
+//         else if(userTransportationInput === "No" || userTransportationInput === "no"){
+//             for (let i = 0; userTransportationInput != "Complete"; i++){
+//             let newTransportation = GetRandomValueFromArray(transportation); // re-randomizes Transportation
+//             let newTransportationInput= prompt("How does " +newTransportation+ " sound? Yes or No");  // prompts user again
+//                 if (newTransportationInput=== "Yes" || newTransportationInput === "yes"){ 
+//                     userTransportationInput = "Complete"; // ends loop when user is ok with choice
+                       
+//                }     
+//         }
+//         }
+
+//            console.log(userTransportationInput);
+
+           
+// userEntertainment= GetRandomValueFromArray(entertainment);
+
+// let userEntertainmentInput= prompt("Are you happy with the entertainment "+userEntertainment+ "? Enter: Yes or No");
+//         if (userEntertainmentInput === "Yes" || userEntertainmentInput === "yes"){
+//              userEntertainmentInput = "Complete";
+//         }
+        
+//         else if(userEntertainmentInput === "No" || userEntertainmentInput === "no"){
+//             for (let i = 0; userEntertainmentInput != "Complete"; i++){
+//             let newEntertainment = GetRandomValueFromArray(entertainment); // re-randomizes Entertainment
+//             let newEntertainmentInput= prompt("How does " +newEntertainment+ " sound? Yes or No");  // prompts user again
+//                 if (newEntertainmentInput=== "Yes" || newEntertainmentInput === "yes"){ 
+//                     userEntertainmentInput = "Complete"; // ends loop when user is ok with choice
+                       
+//                }     
+//         }
+//         }
+
+//            console.log(userEntertainmentInput);
+
